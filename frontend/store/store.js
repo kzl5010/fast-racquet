edadb3108692c94dd786e93ec15db6dbca292a50
Ken <kzl5010@gmail.com>
2017-10-19 10:08:50 -0700

    store, redux tools not working

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,13 +1,15 @@
+// @flow
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'remote-redux-devtools';
 import RootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
 
-const configureStore = (preloadedState = {}) => (
+const configureStore = (preloadedState: {} = {}) => (
   createStore(
     RootReducer,
     preloadedState,
-    applyMiddleware(thunk)
+    composeWithDevTools(applyMiddleware(thunk))
   )
-)
+);
 
 export default configureStore;
