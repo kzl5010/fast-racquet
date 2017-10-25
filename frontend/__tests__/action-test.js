bdaae9ecdd16b0595f54b11a3cc0416ecc157c52
Ken <kzl5010@gmail.com>
2017-10-25 12:01:28 -0700

    modified test, added snapshot for reducer

--- frontend/__tests__/action-test.js
+++ frontend/__tests__/action-test.js
@@ -4,27 +4,27 @@ const mapStateToProps = ({ session }) => {
   if (session) return {
   loggedIn: Boolean(session.currentUser),
   errors: session.errors
-}
+};
 return {};
-}
+};
 
 describe('MapStateToProps', () => {
   it('Maps State to Props', ()=> {
     let initialState = { session: {
       currentUser: true,
       errors: ['bananas']
-    }}
+    }};
     expect(mapStateToProps(initialState)).toEqual({
       loggedIn: true,
       errors: ['bananas']
     })
-  })
+  });
 
   it('It doesn\'t map unwanted props', () => {
     let initialState = {
       oop: 'baboop',
       bimibap: 'bowl',
-    }
+    };
     expect(mapStateToProps(initialState)).toEqual({})
   })
 
--- frontend/__tests__/order_form-test.js
+++ frontend/__tests__/order_form-test.js
@@ -2,7 +2,13 @@ import ordersReducer from '../reducers/orders_reducer';
 
 let initialState = {};
 
-describe('Orders Reducer', ()=> {
+const mapStateToProps = (state, ownProps) => ({
+  currentUser: state.session.currentUser,
+  errors: state.orders.errors,
+  stringies: state.stringies.stringies
+});
+
+describe('Order Container MapStateToProps', ()=> {
    it('doesn\'t do anything for unrelated actions', () => {
      let finalState = ordersReducer(initialState, { type: "COOLIO"});
      expect(finalState).toEqual(initialState);
@@ -10,14 +16,9 @@ describe('Orders Reducer', ()=> {
 
    it('updates the store on an action', () => {
      let order = { id: 5, tv_show: "blainer" };
-     expect(ordersReducer(initialState, { type: "RECEIVE_ORDER", order})).toEqual({5: order})
+     expect(ordersReducer(initialState, { type: "RECEIVE_ORDER", order})).toEqual({'5': order})
    });
 
-  it('updated state matches snapshot', () => {
-    let order = { id: 5, tv_show: "blainer" };
-    console.log(ordersReducer(initialState, { type: "RECEIVE_ORDER", order}));
-    expect(ordersReducer(initialState, { type: "RECEIVE_ORDER", order})).toMatchSnapshot()
-  });
 
   //  it('should greet Users to the site and mention money', () => {
   //      expect(shallow(<Greeting />).contains(<PageHeader className="text-center">Quality string jobs to your door for $40.</PageHeader>)).toBe(true);
