b5150cd3f30021507cfa9e76608116d757c6ff0f
Ken <kzl5010@gmail.com>
2017-10-29 18:30:14 -0700

    redux tools working

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,6 +1,6 @@
 // @flow
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'remote-redux-devtools';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import RootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
 
